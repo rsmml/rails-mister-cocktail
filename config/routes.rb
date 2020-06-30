52c6ab3560ff8984b5388068a008bb858f7e01d6
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-06-30 18:50:47 +0200

    css grid index page done

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cocktails#index'
   resources :cocktails, only: %i[show new create] do
-    resources :doses, only: %i[new create]
+    resources :doses, only: %i[new create show]
   end
   resources :doses, only: %i[destroy]
 end
