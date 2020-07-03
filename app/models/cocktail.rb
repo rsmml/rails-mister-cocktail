4fa19439c2fe4356a4c11ec5651f4f0b82859249
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-07-03 13:25:23 +0200

    stars review done

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,6 @@
 class Cocktail < ApplicationRecord
-  has_many :reviews, dependent: :destroy
   has_many :doses, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   has_many :ingredients, through: :doses
   has_one_attached :photo
 
